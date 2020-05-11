b268d0f2d432d6a47a92ae0f56c8a1a96a28577e
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-11 20:16:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,8 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   valid_calls.each do |call|
     if planeteer_calls.find{|e| e == call}
-      puts call
       return call
     end
   end
+  return nil
 end
